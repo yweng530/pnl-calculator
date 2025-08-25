7714681a74d0ecc063a348bad0a8e2f8978052dc
Yuhua Weng <yuhua2@illinois.edu>
2025-08-25 13:15:15 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pnl-calculator
+
+Run the program:
+
+python pnl_calculator.py [path to the input file] [accounting scheme]
+
+Example: python pnl_calculator.py input.csv fifo
--- pnl_calculator.py
+++ pnl_calculator.py
@@ -11,10 +11,39 @@ def parse_arg():
         sys.stderr.write("Error: scheme must be 'fifo' or 'lifo'")
     return file_path, scheme
 
+def calculate_pnl(hashmap, symbol, type, price, qty, scheme):
+    opposite = 'S' if type == 'B' else 'B'
+    # look up trades for the opposite type
+    trades = hashmap[symbol][opposite] # queue of (qty, price)
+    pnl = 0.0
+    qty_matched = 0
+    remain = qty
+    while trades and remain > 0:
+        if scheme == 'fifo':
+            prev_qty, prev_price = trades.popleft()
+        else:
+            prev_qty, prev_price = trades.pop()
+        qty_took = min(prev_qty, remain)
+        if type == 'S':
+            pnl += (price - prev_price) * qty_took
+        else:
+            pnl += (prev_price - price) * qty_took
+        qty_matched += qty_took
+        remain -= qty_took
+        prev_qty -= qty_took
+        # put the remaining quantity back to the queue
+        if prev_qty > 0:
+            if scheme == 'fifo':
+                trades.appendleft((prev_qty, prev_price))
+            else:
+                trades.append((prev_qty, prev_price))
+    return pnl, qty_matched, remain
+
 def main():
     csv_path, scheme = parse_arg()
 
-    print("TIMESTAMP,SYMBOL,PNL")
+    writer = csv.writer(sys.stdout, lineterminator="\n")
+    writer.writerow(["TIMESTAMP", "SYMBOL", "PNL"])
     hashmap = {} # hashmap[symbol] gives {"B": [list of (quantity, price)], "S": [list of (quantity, price)]}
 
     with open(csv_path, newline="") as f:
@@ -31,6 +60,12 @@ def main():
                 continue
             if symbol not in hashmap:
                 hashmap[symbol] = {"B": deque(), "S": deque()}
+
+            pnl, qty_matched, remain = calculate_pnl(hashmap, symbol, type, price, qty, scheme)
+            if qty_matched > 0:
+                writer.writerow([time, symbol, f"{pnl:.2f}"])
+            if remain > 0:
+                hashmap[symbol][type].append((remain, price))
             
 
 if __name__ == "__main__":
